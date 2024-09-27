21adcd013f220bb84ff030c7fb406f98f4b28f69
Allan Castanier <allan.castanier@student.ehb.be>
2024-09-27 18:55:06 +0200

    Gitlogo klaar

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,2 +1,59 @@
 "use strict";
 
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+
+
+gitLogo();
+
+function gitLogo(){
+
+
+context.beginPath();
+
+
+context.fillStyle="#9933FF";
+
+context.beginPath();
+context.rect(130, 320, 110, 50);
+context.fill();
+
+
+
+
+
+context.fillStyle="#9933FF";
+context.beginPath();
+context.rect(350,320,110,50);
+context.fill();
+
+
+
+
+
+
+context.beginPath();
+
+
+context.fillStyle="#9933FF";
+
+context.beginPath();
+context.rect(180,220,60, 60);
+context.fill();
+
+
+
+
+
+context.fillStyle="#9933FF";
+context.beginPath();
+context.rect(345,220,60,60);
+context.fill();
+
+
+
+}
