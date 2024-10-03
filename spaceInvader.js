47f9eb0bc02cb19990f9c0a04d3c24f765a41f39
Allan Castanier <allan.castanier@student.ehb.be>
2024-10-03 14:45:04 +0200

    Aller laatste opdracht klaar

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,59 +1,33 @@
 "use strict";
 
-
 let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext("2d");
 
-
-
 gitLogo();
 
-function gitLogo(){
+function gitLogo() {
+	context.beginPath();
+	context.rect(105, 140, 300, 300);
+	context.fillStyle = "black";
+	context.fill();
+	context.beginPath();
+	context.fillStyle = "#9933FF";
 
+	context.beginPath();
+	context.rect(130, 320, 100, 50);
+	context.fill();
 
-context.beginPath();
+	context.beginPath();
+	context.rect(280, 320, 100, 50);
+	context.fill();
 
+	context.beginPath();
+	context.rect(180, 220, 50, 50);
+	context.fill();
 
-context.fillStyle="#9933FF";
-
-context.beginPath();
-context.rect(130, 320, 110, 50);
-context.fill();
-
-
-
-
-
-context.fillStyle="#9933FF";
-context.beginPath();
-context.rect(350,320,110,50);
-context.fill();
-
-
-
-
-
-
-context.beginPath();
-
-
-context.fillStyle="#9933FF";
-
-context.beginPath();
-context.rect(180,220,60, 60);
-context.fill();
-
-
-
-
-
-context.fillStyle="#9933FF";
-context.beginPath();
-context.rect(345,220,60,60);
-context.fill();
-
-
-
+	context.beginPath();
+	context.rect(280, 220, 50, 50);
+	context.fill();
 }
